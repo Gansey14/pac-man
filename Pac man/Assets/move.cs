088f0b1eb0327ac9fe7f4a56397927a6d67b99d5
EAAA account <eaasahl@students.eaaa.dk>
2025-03-28 11:37:54 +0100

    Update move.cs

--- Pac man/Assets/move.cs
+++ Pac man/Assets/move.cs
@@ -1,39 +1,41 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    public bool collision = false;
+
+    void moveRight()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            while (collision == false)
+            {
+                Vector3 position = transform.position;
+                position.x += 0.1f;
+                transform.position = position;
+            }
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+    }
 
+    void onCollisionEnter2D(Collision2D other)
+    {
+        collision = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * 10);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * Time.deltaTime * 10);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * 10);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * 10);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * 10);
-        }
+        moveRight();
     }
+
+
 }
+
+
